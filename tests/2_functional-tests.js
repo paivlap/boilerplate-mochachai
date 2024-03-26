5b77ebe75888079cb2f1d441ae0e70b3887e124c
paivlap <164074812+paivlap@users.noreply.github.com>
2024-03-26 14:51:38 +0200

    Update 2_functional-tests.js

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -58,7 +58,10 @@ suite('Functional Tests', function () {
           "surname": "da Verrazzano"
         })
         .end(function (err, res) {
-      assert.fail();
+      assert.equal(res.status, 200, 'response status should be 200');
+      assert.equal(res.type, 'application/json',  'Response should be json');
+      assert.equal(res.body.name, 'Giovanni');
+      assert.equal(res.body.surname, 'da Verrazzano');
 
       done();
     });
